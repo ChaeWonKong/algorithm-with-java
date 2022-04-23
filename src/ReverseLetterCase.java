0651a653bb34100c434ab938658f18884a890376
Kong Chae Won <chaewonkong@gmail.com>
2022-04-23 13:30:04 +0900

    feat: 🎸 reverse string case

--- src/ReverseLetterCase.java
+++ src/ReverseLetterCase.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ReverseLetterCase {
+import java.util.Scanner;
+
+public class ReverseLetterCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(reverseLetterCase(sc.nextLine()));
+        sc.close();
+    }
+
+    private static String reverseLetterCase(String s) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            char target = s.charAt(i);
+            if (Character.isLowerCase(target)) {
+                result[i] = Character.toUpperCase(target);
+            } else {
+                result[i] = Character.toLowerCase(target);
+            }
+        }
+        return new String(result);
+    }
 }
